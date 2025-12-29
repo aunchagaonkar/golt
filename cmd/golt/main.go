3f49e45ddc5390dd471630946103a52c7f537a07
Ameya Unchagaonkar <ameya21unchagaonkar@gmail.com>
2025-12-29 08:13:55 +0530

    refactor main function to add command line flags and add HTTP gateway support in main.go

--- cmd/golt/main.go
+++ cmd/golt/main.go
@@ -8,13 +8,16 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/aunchagaonkar/golt/api"
+	"github.com/aunchagaonkar/golt/logger"
 	"github.com/aunchagaonkar/golt/raft"
 )
 
 func main() {
 	id := flag.String("id", "", "Unique node ID (required)")
-	address := flag.String("address", "", "Address to listen on, e.g. localhost:8001 (required)")
-	peers := flag.String("peers", "", "Comma-separated list of peer addresses, e.g. localhost:8002,localhost:8003")
+	address := flag.String("address", "", "Raft address to listen on, e.g. localhost:7001 (required)")
+	httpAddr := flag.String("http", "", "HTTP API address, e.g. localhost:8001 (optional)")
+	peers := flag.String("peers", "", "Comma-separated list of peer addresses, e.g. localhost:7002,localhost:7003")
 	dataDir := flag.String("data", "", "Directory for storing data")
 	flag.Parse()
 
@@ -27,6 +30,9 @@ func main() {
 	if *dataDir == "" {
 		*dataDir = "/tmp/golt-" + *id
 	}
+
+	logger.Init(*id)
+
 	var peerList []string
 	if *peers != "" {
 		peerList = strings.Split(*peers, ",")
@@ -45,11 +51,24 @@ func main() {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 
+	var gateway *api.Gateway
+	if *httpAddr != "" {
+		gateway = api.NewGateway(server, *httpAddr)
+		if err := gateway.Start(); err != nil {
+			log.Fatalf("Failed to start HTTP gateway: %v", err)
+		}
+		log.Printf("HTTP API available at http://%s", *httpAddr)
+	}
+
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
 
 	sig := <-sigCh
 	log.Printf("Received signal %v, shutting down...", sig)
+
+	if gateway != nil {
+		gateway.Stop()
+	}
 
 	server.Stop()
 	log.Printf("Node %s stopped", *id)

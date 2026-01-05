8757dc4d98bc53453beb6847b7a077632cbda1ac
Ameya Unchagaonkar <ameya21unchagaonkar@gmail.com>
2026-01-05 16:04:16 +0530

    add CORS support to HTTP gateway

--- api/gateway.go
+++ api/gateway.go
@@ -31,9 +31,21 @@ func (g *Gateway) Start() error {
 	mux.HandleFunc("/get", g.handleGet)
 	mux.HandleFunc("/status", g.handleStatus)
 
+	cors := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		mux.ServeHTTP(w, r)
+	})
+
 	g.httpServer = &http.Server{
 		Addr:    g.httpAddr,
-		Handler: mux,
+		Handler: cors,
 	}
 
 	logger.Info().Str("addr", g.httpAddr).Msg("Starting HTTP gateway")
